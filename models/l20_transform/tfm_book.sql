8d8e2b51c2cf5c59d1e1498e179b1ac37292abde
nikiljk <nikiljk@gmail.com>
2024-12-17 13:43:53 +0100

    made minor changes so as to adapt to the changes in the seed data

--- models/l20_transform/tfm_book.sql
+++ models/l20_transform/tfm_book.sql
@@ -1,3 +1,11 @@
-{{ dbt_utils.union_relations(
-    relations=[ref('manual_book1'), ref('manual_book2')]
-) }}
+WITH combined AS (
+    {{ dbt_utils.union_relations(
+        relations=[ref('manual_book1'), ref('manual_book2')]
+    ) }}
+)
+SELECT Book,Date,Trader,Instrument,Action,Cost,Currency,Volume,Cost_Per_Share,
+       CASE 
+           WHEN stock_exchange_name = 'NASDAQ' THEN 'NASDAQ CAPITAL MARKET'
+           ELSE stock_exchange_name
+       END AS stock_exchange_name
+FROM combined
